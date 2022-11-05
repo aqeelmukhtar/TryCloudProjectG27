0748f4107ae2bc7ebbc33a9bdcf24204027c7c5b
Alpimax <alperrozkann@gmail.com>
2022-11-05 09:48:47 -0400

    login updated

--- src/test/java/com/trycloud/step_definitions/Login_step_def.java
+++ src/test/java/com/trycloud/step_definitions/Login_step_def.java
@@ -2,11 +2,13 @@ package com.trycloud.step_definitions;
 
 import com.trycloud.pages.BasePage;
 import com.trycloud.pages.Login;
+import com.trycloud.utilities.ConfigurationReader;
 import com.trycloud.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class Login_step_def extends BasePage {
     Login login = new Login();
@@ -15,40 +17,23 @@ public class Login_step_def extends BasePage {
     @Given("user on the login page")
     public void user_on_the_login_page() {
 
-        Driver.getDriver().get("http://qa3.trycloud.net");
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
 
     }
     @When("user use username {string} and passcode {string}")
     public void userUseUsernameAndPasscode(String arg0, String arg1) {
        login.username.sendKeys(arg0);
        login.password.sendKeys(arg1);
-
     }
-
 
     @When("user click the login button")
     public void user_click_the_login_button() {
         login.loginButton.click();
 
     }
-    @Then("verify the user should be at the dashboard page")
-    public void verify_the_user_should_be_at_the_dashboard_page() {
-
-    }
-
-
-    @And("user click the dashboard")
-    public void userClickTheDashboard() {
-
-
-        contacts.click();
-        dashboard.click();
-        mail.click();
-
-
-
-
-
+    @Then("verify the user should be at the {string} page")
+    public void verify_the_user_should_be_at_the_dashboard_page(String string) {
+        Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
     }
 
 
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -4,10 +4,8 @@ Feature: As a user, I should be able to log in.
   Scenario Outline: Verify login with valid credentials
     Given user on the login page
     When user use username "<username>" and passcode "<password>"
-
     And user click the login button
-    Then verify the user should be at the dashboard page
-    And user click the dashboard
+    Then verify the user should be at the "dashboard" page
 
     Examples:
       | username | password    |
