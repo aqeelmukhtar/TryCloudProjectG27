7e8a322907215237b9beba8abdbe7c9b65cbc464
dzvisa1 <dzvisaa@gmail.com>
2022-11-14 23:16:24 -0500

    us-3 story updated.

--- src/test/java/com/trycloud/pages/Dashboard.java
+++ src/test/java/com/trycloud/pages/Dashboard.java
@@ -11,27 +11,27 @@ import java.util.List;
 
 public class Dashboard extends BasePage {
 
-//    public Dashboard() {
-//        PageFactory.initElements(Driver.getDriver(), this);
-//    }
-//
-//    @FindBy(xpath = "//ul[@id='appmenu']/li")
-//    public List<WebElement> modules;
-//
-//
-//    public List<String> getModulesText(){
-//        List<String> modulesText=new ArrayList<>();
-//        for (int i = 0; i < modules.size()-1; i++) {
-//
-//            BrowserUtils.hover(modules.get(i));
-//            BrowserUtils.sleep(1);
-//            modulesText.add(modules.get(i).getText());
-//
-//        }
-//
-//        return modulesText;
-//
-//    }
-//
+    public Dashboard() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//ul[@id='appmenu']/li")
+    public List<WebElement> modules;
+
+
+    public List<String> getModulesText(){
+        List<String> modulesText=new ArrayList<>();
+        for (int i = 0; i < modules.size()-1; i++) {
+
+            BrowserUtils.hover(modules.get(i));
+            BrowserUtils.sleep(1);
+            modulesText.add(modules.get(i).getText());
+
+        }
+
+        return modulesText;
+
+    }
+
 
 }
--- src/test/java/com/trycloud/step_definitions/Dashboard_StepDef.java
+++ src/test/java/com/trycloud/step_definitions/Dashboard_StepDef.java
@@ -11,23 +11,23 @@ import java.util.List;
 
 public class Dashboard_StepDef {
 
-//    Login login=new Login();
-//
-//    @When("user  log in with valid credentials")
-//    public void user_log_in_with_valid_credentials() {
-//      login.autoLogin();
-//
-//
-//    }
-//    @Then("Verify the user see the following modules:")
-//    public void verify_the_user_see_the_following_modules(List<String> expectedModuleNames) {
-//
-//           List<String> actualModuleNames = new Dashboard().getModulesText();
-//          Assert.assertEquals(expectedModuleNames,actualModuleNames);
-//       // Assert.assertEquals(expectedModuleNames, BrowserUtils.getElementsText(new Dashboard().modules));
-//
-//    }
-//
+    Login login=new Login();
+
+    @When("user  log in with valid credentials")
+    public void user_log_in_with_valid_credentials() {
+      login.autoLogin();
+
+
+    }
+    @Then("Verify the user see the following modules:")
+    public void verify_the_user_see_the_following_modules(List<String> expectedModuleNames) {
+
+           List<String> actualModuleNames = new Dashboard().getModulesText();
+          Assert.assertEquals(expectedModuleNames,actualModuleNames);
+       // Assert.assertEquals(expectedModuleNames, BrowserUtils.getElementsText(new Dashboard().modules));
+
+    }
+
 
 
 }
